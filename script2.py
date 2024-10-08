7412ee15a73178cb254c55e0bd25bfb3e5e50469
Janusz Przewocki <janusz.przewocki@ug.edu.pl>
2024-10-08 19:10:21 +0200

    Added error handling in script2.py

--- script2.py
+++ script2.py
@@ -1,4 +1,9 @@
-number = int(input('Enter a natural number: '))
+try:
+    number = int(input('Enter a natural number: '))
+except ValueError:
+    print('You must enter a natural number')
+    exit(1)
+
 if number % 2 == 0:
     print('Even number')
 else:
