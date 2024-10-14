5bb4b12dae85c8d2c7a964782154bbc0c4cf31b3
Janusz Przewocki <janusz.przewocki@ug.edu.pl>
2024-10-14 13:46:36 +0200

    Added Banach's birthday to scrpit1.py, version 2

--- script1.py
+++ script1.py
@@ -2,3 +2,7 @@ from datetime import date
 
 print('Hello world!')
 print('Today is:', date.today().strftime('%d-%m-%Y'))
+
+banach_birth = date(1892, 3, 30)
+print('It is', (date.today() - banach_birth).days,
+      "days from Banach's birth.")
