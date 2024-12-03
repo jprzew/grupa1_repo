ea780e1d3d554661048608fdb9f4b003161d7e84
Janusz Przewocki <janusz.przewocki@ug.edu.pl>
2024-12-03 19:17:29 +0100

    Started to prepare iris-download

---
+++ src/download.py
@@ -0,0 +1,14 @@
+from sklearn import datasets
+import pandas as pd
+
+
+COLUMN_NAMES = ['sepal_length',
+                'sepal_width',
+                'petal_length',
+                'petal_width']
+
+iris = datasets.load_iris()
+df = pd.DataFrame(iris['data'])
+df.columns = COLUMN_NAMES
+print(df)
+
